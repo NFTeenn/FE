8ab18836ab8fe3e27bec841bff46892b745c7959
Dino0204 <s24020@gsm.hs.kr>
2025-09-25 23:42:12 +0900

    feat: tanstack provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import TanstackProvider from "./_provider/tanstackProvider";
 
 export const metadata: Metadata = {
-  title: "NFTeen",
-  description: "NFT Platform for Teens",
+	title: "NFTeen",
+	description: "NFT Platform for Teens",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html lang="ko">
-      <body >
-        <main className="min-h-screen bg-gray-50">{children}</main>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="ko">
+			<body>
+				<TanstackProvider>
+					<main className="min-h-screen bg-gray-50">{children}</main>
+				</TanstackProvider>
+			</body>
+		</html>
+	);
 }
