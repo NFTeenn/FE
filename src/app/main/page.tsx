e9133c6a9a8587df2751c38aa68d9675c60b50a9
Dino0204 <s24020@gsm.hs.kr>
2025-11-11 15:01:49 +0900

    Merge branch 'develop' of https://github.com/NFTeenn/FE into feature/auth

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,20 +1,15 @@
 
 export default function Main() {
-<<<<<<< HEAD
-	return <div>main layout</div>;
-}
-=======
     return (
         <>
             <div id="container" className="w-[100vw] h-[100vh] bg-brand-bg">
                 <div id="content">
-                    
+
                 </div>
                 <div id="sideContent">
-                    
+
                 </div>
             </div>
         </>
     );
 }
->>>>>>> aad35fe9fcf8983fe7b13422760cbe41a2be73c2
