58b96e940de689b359b91183deaf288bc2d86d54
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-11-11 14:03:28 +0900

    feat: 사이드 바 개발

--- src/widgets/sidebar/layout.tsx
+++ src/widgets/sidebar/layout.tsx
@@ -1,36 +1,40 @@
 "use client";
 
-import globalStyle from "@/styles/global.module.css";
-import { usePathname } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 export default function Sidebar() {
-    const pathname = usePathname();
+  const router = useRouter();
 
-    const menuItems = [
-        { name: '메인페이지', path: '/main' },
-        { name: '경제사전', path: '/dictionary' },
-        { name: '경제뉴스', path: '/news' },
-        { name: '돈돈이 & 마이페이지', path: '/mypage' },
-    ];
+  const menuItems = [
+    { name: '메인페이지', path: '/main' },
+    { name: '경제사전', path: '/dictionary' },
+    { name: '경제뉴스', path: '/news' },
+    { name: '돈돈이 & 마이페이지', path: '/mypage' },
+  ];
 
     return (
-        <>
-            <div id="sidebar" className={globalStyle.sidebar}>
-                <div id="logo" className={globalStyle.logo}></div>
-                <div id="menu" className={globalStyle.menu}>
-                    <ul className="flex flex-col gap-[2vh] w-full items-center ">
-                        {menuItems.map((item) => (
-                            <li key={item.name}
-                                className={`${globalStyle.menuLi} ${pathname === item.path ? globalStyle.active : ''}`}>
-                                {item.name}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div id="logout" className={globalStyle.logout}>
-                    <button className="w-[80%] h-[2.2rem] rounded-[8px] border-[1px] border-[#00000033] cursor-pointer">로그아웃</button>
-                </div>
+        <div className="fixed top-0 left-0 h-screen w-64 bg-white border-r border-gray-300 flex flex-col items-center pt-10">
+            {/* 로고 */}
+            <div className="w-full h-24 flex justify-center items-center">
+                <div className="w-20 h-20 bg-gray-200 rounded-full"></div>
             </div>
-        </>
-    );  
+
+            {/* Menu */}
+            <div className="w-4/5 mt-10">
+                <ul className="flex flex-col gap-4 w-full items-center">
+                    {menuItems.map((item) => (
+                        <li key={item.name} onClick={() => router.push(item.path)}
+                        className="w-full h-12 flex justify-center items-center rounded-xl cursor-pointer hover:bg-gray-100 transition-colors">
+                            {item.name}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Logout */}
+            <div className=" w-full flex justify-center absolute bottom-10">
+                <button className="w-4/5 h-9 rounded-lg border border-gray-300 text-black font-medium hover:bg-gray-100 transition-colors cursor-pointer">로그아웃</button>
+            </div>
+        </div>
+    );
 }
