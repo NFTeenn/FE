f3709e38c224a7b3c6d963f197d32889484afdfa
Dino0204 <s24020@gsm.hs.kr>
2025-12-12 12:19:19 +0900

    feat: star svg

---
+++ src/shared/assets/star.tsx
@@ -0,0 +1,33 @@
+export default function Star({
+  className = "",
+  onClick,
+  color = "#FFD63A"
+}: {
+  className?: string;
+  onClick?: () => void;
+  color?: string;
+}) {
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+      onClick={onClick}
+    >
+      <path
+        d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z"
+        fill={color}
+        stroke={color}
+        stroke-width={2}
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  )
+}
+
+
+
