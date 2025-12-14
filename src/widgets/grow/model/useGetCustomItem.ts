d24e60a56eb057c7dad33e1448b45e29eb7b7db6
Dino0204 <s24020@gsm.hs.kr>
2025-12-14 23:58:00 +0900

    feat: 상점 API

---
+++ src/widgets/grow/model/useGetCustomItem.ts
@@ -0,0 +1,9 @@
+import { useQuery } from "@tanstack/react-query"
+import { getCustomItem } from "../api/getCustomItem"
+
+export const useGetCustomItem = () => {
+    return  useQuery({
+      queryKey: ["custom-item"],
+      queryFn: () => getCustomItem(),
+    })
+}
