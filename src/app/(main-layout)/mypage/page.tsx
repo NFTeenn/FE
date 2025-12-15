922f76447eaa9cc4ec45f1483d7bc65bbb06eecb
Dino0204 <s24020@gsm.hs.kr>
2025-12-15 19:59:07 +0900

    feat: Operation에 맞게 데이터 반환

--- src/app/(main-layout)/mypage/page.tsx
+++ src/app/(main-layout)/mypage/page.tsx
@@ -1,22 +1,57 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 import bowl from "src/shared/assets/bowl.svg";
-import dondon from "src/shared/assets/mypage_dondon.svg";
-import storage from "src/shared/assets/storage.svg";
 import medal from "src/shared/assets/medal.svg";
+import dondon from "src/shared/assets/mypage_dondon.svg";
 import shop from "src/shared/assets/shop.svg";
+import storage from "src/shared/assets/storage.svg";
 import MyPageSidebar from "@/widgets/sidebar/ui/myPageSidebar";
 
+export type MyPageSidebarOperation = "STORAGE" | "ACHIEVEMENT" | "SHOP";
+
+interface Card {
+  id: number;
+  img: string;
+  label: string;
+  size: number;
+  method: MyPageSidebarOperation;
+}
+
+const cardList: Card[] = [
+  {
+    id: 1,
+    img: storage,
+    label: "독립한 돈돈",
+    size: 50,
+    method: "STORAGE",
+  },
+  {
+    id: 2,
+    img: medal,
+    label: "업적",
+    size: 45,
+    method: "ACHIEVEMENT",
+  },
+  {
+    id: 3,
+    img: shop,
+    label: "상점",
+    size: 40,
+    method: "SHOP",
+  },
+];
+
 export default function MyPage() {
+  const [sidebarOperation, setSidebarOperation] = useState<MyPageSidebarOperation | null>(null);
+
   return (
-    <div className="w-full h-screen flex flex-col overflow-hidden fixed">
+    <div className="w-full h-screen flex flex-col">
+      <div className="flex w-full bg-brand-b4 h-[70vh]">
+        <div className="w-full flex justify-between m-12">
 
-      {/* 상단 영역 */}
-      <div className="flex w-full bg-brand-b4 relative h-[70vh]">
-        <div className="absolute inset-0 pointer-events-none">
-
-          {/* 즐겨찾기 박스 */}
-          <div className="absolute top-[3vh] left-1/2 -translate-x-[45vw] flex w-[40%] z-[99]">
-
+          <div className="flex w-240">
             <div className="flex-1 h-20  bg-white border border-gray-300 rounded-l-2xl" />
             <div className="flex-1 h-20  bg-white border border-gray-300" />
             <div className="flex-1 h-20  bg-white border border-gray-300 rounded-r-2xl flex items-center justify-center">
@@ -24,74 +59,87 @@ export default function MyPage() {
             </div>
           </div>
 
-          <div className="absolute right-0  bottom-[3vw] mr-[20vw] flex flex-col items-center gap-[2vw] z-[999]">
-
-            {/* 공통 카드 컴포넌트 구조 */}
-            {[
-              { img: storage, label: "독립한 돈돈", size: 50 },
-              { img: medal, label: "업적", size: 45 },
-              { img: shop, label: "상점", size: 40 },
-            ].map((item, i) => (
-              <div key={i} className="flex flex-col items-center">
-                <Image src={item.img} alt={item.label} width={item.size} height={item.size} className="w-[4rem] z-10" />
-                <div className="w-20 h-12 bg-white border border-black/20 rounded-xl -mt-3 
-                                flex items-center justify-center text-sm">
+          <div className="flex flex-col gap-4">
+            {cardList.map((item) => (
+              <button
+                type="button"
+                key={item.id}
+                className="flex flex-col items-center cursor-pointer"
+                onClick={() => {
+                  setSidebarOperation(item.method);
+                }}
+              >
+                <Image
+                  src={item.img}
+                  alt={item.label}
+                  width={item.size}
+                  height={item.size}
+                  className="w-[4rem] z-10"
+                />
+                <div className="w-20 h-12 bg-white border border-black/20 rounded-xl -mt-3 flex items-center justify-center text-sm">
                   {item.label}
                 </div>
-              </div>
+              </button>
             ))}
-
           </div>
         </div>
 
-        {/* 캐릭터 */}
-        <div className="absolute bottom-[-5%] left-1/2 -translate-x-1/2 z-[999] ">
-          <Image src={dondon} alt="dondon" className="w-[25vw] h-auto pointer-events-none"
-          />
-        </div>
-
-        {/* Bowl */}
-        <div className="absolute bottom-[-10%] left-[20%] -translate-x-1/2 z-[999] ">
-          <Image src={bowl} alt="bowl" className="w-[19vw] h-auto pointer-events-none" />
-        </div>
       </div>
 
+      <div className="flex flex-col justify-end bg-white flex-1 pb-[3rem] px-[6rem]">
+        <section className="relative flex flex-1 h-full">
+          <div className="absolute -top-5 left-30">
+            <Image
+              src={bowl}
+              alt="bowl"
+              className="w-[19vw]"
+            />
+          </div>
 
-      {/* 하단 정보 박스 */}
-      <div className="w-full bg-white h-[30vh]
-                      py-8 px-6 sm:px-10 md:px-16 flex flex-col  gap-8 relative">
+          <div className="absolute -top-60 right-30">
+            <Image
+              src={dondon}
+              alt="dondon"
+              className="w-[25vw]"
+            />
+          </div>
 
-        <div className="flex flex-row w-full items-start gap-6 mt-[2%]">
+        </section>
+        <section className="flex flex-1 items-end ">
+          <article className="flex flex-1 items-start gap-6">
 
-          {/* 누적 코인 */}
-          <div className="flex flex-col w-[13%] ">
-            <span className="text-gray-500 mb-2">누적된 코인:</span>
-            <div className="h-10 border border-gray-300 rounded-3xl px-4 flex items-center">
-              <div className="w-5 h-5 bg-brand-main rounded-full" />
+            <div className="flex flex-col min-w-40">
+              <span className="text-gray-500 mb-2">누적된 코인:</span>
+              <div className="h-10 border border-gray-300 rounded-3xl px-4 flex items-center">
+                <div className="w-5 h-5 bg-brand-main rounded-full" />
+              </div>
             </div>
-          </div>
 
-          {/* 성장도 */}
-          <div className="flex flex-col w-[30%]">
-            <span className="text-gray-500 mb-2">성장도:</span>
-            <div className="h-10 border border-gray-300 rounded-3xl px-4 flex items-center">
-              <div className="w-5 h-5 bg-brand-main rounded-full" />
+            <div className="flex flex-col min-w-120">
+              <span className="text-gray-500 mb-2">성장도:</span>
+              <div className="h-10 border border-gray-300 rounded-3xl px-4 flex items-center">
+                <div className="w-5 h-5 bg-brand-main rounded-full" />
+              </div>
             </div>
-          </div>
-        </div>
+          </article>
 
-        {/* 돈돈 이름 박스 */}
-        <div className="absolute bottom-[30%] left-1/2 -translate-x-1/2 ml-[20%]">
-          <div className="w-[6rem] h-[1.5rem] bg-brand-main rounded-t-md flex items-center justify-center mx-auto">
-            돈돈의 이름
-          </div>
-          <div className="w-[10rem] h-[3.5rem] bg-white border border-gray-300 rounded-xl flex items-center justify-center">
-            이름
-          </div>
-        </div>
+          <article className="flex flex-col">
+            <div className="w-[6rem] h-[1.5rem] bg-brand-main rounded-t-md flex items-center justify-center">
+              돈돈의 이름
+            </div>
+            <div className="w-[10rem] h-[3.5rem] bg-white border border-gray-300 rounded-xl flex items-center justify-center">
+              이름
+            </div>
+          </article>
+        </section>
+
       </div>
-      <MyPageSidebar />
-
+      {sidebarOperation && (
+        <MyPageSidebar
+          operation={sidebarOperation}
+          setSidebarOpen={setSidebarOperation}
+        />
+      )}
     </div>
   );
 }
--- src/widgets/sidebar/ui/myPageSidebar.tsx
+++ src/widgets/sidebar/ui/myPageSidebar.tsx
@@ -1,76 +1,118 @@
-"use client"
+"use client";
 
+import type { MyPageSidebarOperation } from "@/app/(main-layout)/mypage/page";
 import X from "@/shared/assets/x";
 import { useGetAchievement } from "@/widgets/grow/model/useGetAchievement";
 import { useGetCustomItem } from "@/widgets/grow/model/useGetCustomItem";
 import { useGetHallOfFame } from "@/widgets/grow/model/useGetHallOfFame";
 
-export default function MyPageSidebar() {
+const Storage = () => {
+  const { data: dondons } = useGetHallOfFame();
 
-  const { data: dondons = [{
-    gen: 1,
-    nickname: "서정민",
-    level: 3,
-    enterDate: "2025-09-17",
-    graduationDate: "2025-09-17",
-    style: 1
-  }, {
-    gen: 2,
-    nickname: "정민이",
-    level: 12,
-    enterDate: "2025-09-17",
-    graduationDate: "2025-09-18",
-    style: 3
-  }] } = useGetHallOfFame();
+  if (!dondons?.length)
+    return <p className="text-xl text-black/40">졸업한 돈돈이 없습니다.</p>;
 
-  const { data: achievements = [{
-    code: "FIRST_DONDON",
-    title: "첫돈돈",
-    description: "돈돈이를 처음 분양했어요!",
-    achieved: true
-  }] } = useGetAchievement();
+  return (
+    <>
+      {dondons?.map((dondon) => (
+        <article key={dondon.gen} className="flex flex-col w-fit">
+          <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
+          <p>{dondon.gen}대 돈돈</p>
+          <b>{dondon.nickname}</b>
+          <small>
+            {dondon.enterDate} ~ {dondon.graduationDate}
+          </small>
+        </article>
+      ))}
+    </>
+  );
+};
 
-  const { data: customItems = [{
-    id: 1,
-    name: "반짝이 모자",
-    description: "특별한 모자",
-    price: 100
-  }] } = useGetCustomItem();
+const Achievement = () => {
+  const { data: achievements } = useGetAchievement();
 
+  if (!achievements?.length)
+    return <p className="text-xl text-black/40">업적이 없습니다.</p>;
+
+  return (
+    <>
+      {achievements?.map((achievement) => (
+        <article
+          key={achievement.code}
+          className="flex flex-col items-center w-fit"
+        >
+          <p>{achievement.code}</p>
+          <b>{achievement.title}</b>
+          <small>{achievement.description}</small>
+        </article>
+      ))}
+    </>
+  );
+};
+
+const Shop = () => {
+  const { data: customItems } = useGetCustomItem();
+
+  if (!customItems?.length)
+    return <p className="text-xl text-black/40">상품이 없습니다.</p>;
+
+  return (
+    <>
+      {customItems?.map((customItem) => (
+        <article
+          key={customItem.id}
+          className="flex flex-col items-center w-fit border border-black/20 rounded-2xl p-4"
+        >
+          <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
+          <p>{customItem.id}</p>
+          <b>{customItem.name}</b>
+          <b>{customItem.price}C</b>
+          <small>{customItem.description}</small>
+          <p className="cursor-pointer">구매하기</p>
+        </article>
+      ))}
+    </>
+  );
+};
+
+const titles = {
+  STORAGE: "졸업한 돈돈",
+  ACHIEVEMENT: "업적",
+  SHOP: "상점",
+};
+
+export default function MyPageSidebar({
+  operation,
+  setSidebarOpen,
+}: {
+  operation: MyPageSidebarOperation;
+  setSidebarOpen: (operation: MyPageSidebarOperation | null) => void;
+}) {
   return (
     <div className="fixed z-50 top-0 right-0 px-8 py-6 min-w-max h-full space-y-8 bg-white">
       <section className="flex justify-between">
-        <h3>졸업한 돈돈</h3>
-        <X className="cursor-pointer w-4 h-4" onClick={() => { }} />
+        <h3>{titles[operation]}</h3>
+        <X
+          className="cursor-pointer w-4 h-4"
+          onClick={() => {
+            setSidebarOpen(null);
+          }}
+        />
       </section>
       <section className="grid grid-cols-2 gap-8">
-        {dondons?.map((dondon) => (
-          <article key={dondon.gen} className="flex flex-col w-fit">
-            <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
-            <p>{dondon.gen}대 돈돈</p>
-            <b>{dondon.nickname}</b>
-            <small>{dondon.enterDate} ~ {dondon.graduationDate}</small>
-          </article>
-        ))}
-        {achievements?.map((achievement) => (
-          <article key={achievement.code} className="flex flex-col items-center w-fit">
-            <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
-            <p>{achievement.code}</p>
-            <b>{achievement.title}</b>
-            <small>{achievement.description}</small>
-          </article>
-        ))}
-        {customItems?.map((customItem) => (
-          <article key={customItem.id} className="flex flex-col items-center w-fit border border-black/20 rounded-2xl p-4">
-            <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
-            <p>{customItem.id}</p>
-            <b>{customItem.name}</b>
-            <b>{customItem.price}C</b>
-            <small>{customItem.description}</small>
-            <p className="cursor-pointer">구매하기</p>
-          </article>
-        ))}
+        {(() => {
+          switch (operation) {
+            case "STORAGE":
+              return <Storage />;
+            case "ACHIEVEMENT":
+              return <Achievement />;
+            case "SHOP":
+              return <Shop />;
+            default:
+              return null;
+          }
+        })()}
       </section>
     </div>
-  )
+  );
 }
