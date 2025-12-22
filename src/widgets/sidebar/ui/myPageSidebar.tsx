15c6a70da29d79da1ccc240f368d9e7c1829933a
Dino0204 <s24020@gsm.hs.kr>
2025-12-22 15:44:02 +0900

    feat: text-color, layout 수정

--- src/widgets/sidebar/ui/myPageSidebar.tsx
+++ src/widgets/sidebar/ui/myPageSidebar.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import Image from "next/image";
+import achievement_logo from "src/shared/assets/achievement_logo.svg";
+import dondon_logo from "src/shared/assets/mypage_dondon.svg";
+import santas_hat from "src/shared/assets/santas_hat.svg";
 import type { MyPageSidebarOperation } from "@/app/(main-layout)/mypage/page";
 import X from "@/shared/assets/x";
 import { useGetAchievement } from "@/widgets/grow/model/useGetAchievement";
 import { useGetCustomItem } from "@/widgets/grow/model/useGetCustomItem";
 import { useGetHallOfFame } from "@/widgets/grow/model/useGetHallOfFame";
-import Image from "next/image";
-import dondon_logo from "src/shared/assets/mypage_dondon.svg";
-import achievement_logo from "src/shared/assets/achievement_logo.svg";
 
 const Storage = () => {
   const { data: dondons } = useGetHallOfFame();
@@ -19,8 +20,8 @@ const Storage = () => {
     <>
       {dondons?.map((dondon) => (
         <article key={dondon.gen} className="flex flex-col w-fit">
-          <div className="bg-brand-b1 min-w-max aspect-[16/10] rounded-2xl">
-            <Image src={dondon_logo} alt="dondon" />
+          <div className="bg-brand-b3 aspect-[16/10] py-6 px-16 rounded-2xl">
+            <Image src={dondon_logo} alt="dondon" className="object-cover" />
           </div>
           <p>{dondon.gen}대 돈돈</p>
           <b>{dondon.nickname}</b>
@@ -47,9 +48,8 @@ const Achievement = () => {
           className="flex flex-col items-center w-fit"
         >
           <Image src={achievement_logo} alt="achievement" />
-          <p>{achievement.code}</p>
           <b>{achievement.title}</b>
-          <small>{achievement.description}</small>
+          <small className="text-black/40">{achievement.description}</small>
         </article>
       ))}
     </>
@@ -67,14 +67,18 @@ const Shop = () => {
       {customItems?.map((customItem) => (
         <article
           key={customItem.id}
-          className="flex flex-col items-center w-fit border border-black/20 rounded-2xl p-4"
+          className="flex flex-col items-center w-fit border border-black/20 rounded-2xl"
         >
-          <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
-          <p>{customItem.id}</p>
-          <b>{customItem.name}</b>
-          <b>{customItem.price}C</b>
-          <small>{customItem.description}</small>
-          <p className="cursor-pointer">구매하기</p>
+          <div className="w-full px-8 py-4 flex flex-col items-center">
+            <div className="bg-gray-500 min-w-max aspect-[16/10] rounded-2xl"></div>
+            <Image src={santas_hat} alt="santa" />
+            <b>{customItem.name}</b>
+            <small className="text-black/40">{customItem.description}</small>
+            <b className="text-[#fb923c]">{customItem.price}C</b>
+          </div>
+          <span className="w-full text-center cursor-pointer border-t border-black/20 p-2">
+            구매하기
+          </span>
         </article>
       ))}
     </>
@@ -105,7 +109,7 @@ export default function MyPageSidebar({
           }}
         />
       </section>
-      <section className="grid grid-cols-2 gap-8">
+      <section className="grid grid-cols-2 gap-8 min-w-sm max-w-xl">
         {(() => {
           switch (operation) {
             case "STORAGE":
