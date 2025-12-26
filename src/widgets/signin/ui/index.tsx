a2aaf3b76db4d8af99bd9227901bf38d887ff882
Dino0204 <s24020@gsm.hs.kr>
2025-12-27 05:09:15 +0900

    remove: 사용되지 않는 임시 인증 API

--- src/widgets/signin/ui/index.tsx
+++ src/widgets/signin/ui/index.tsx
@@ -1,24 +1,15 @@
 "use client";
 
 import Image from "next/image";
+import { DondonArtwork } from "@/shared/assets/dondon_artwork";
 import google_logo from "@/shared/assets/google_logo.svg";
-import { authCheck } from "../api";
 
 export default function Signin() {
-
-	const handleAuthCheck = async () => {
-		try {
-			const result = await authCheck();
-			console.log("Auth check result:", result);
-
-		} catch (error) {
-			console.error("❌ 예상치 못한 에러:", error);
-		}
-	};
-
 	return (
 		<div className="flex w-[100vw] h-[100vh] relative pl-10 py-10 bg-white">
-			<div className="basis-1/2 h-[90vh] bg-brand-main rounded-[12px]"></div>
+			<div className="flex justify-start items-end overflow-hidden basis-1/2 h-[90vh] bg-brand-main rounded-[12px]">
+				<DondonArtwork className="w-full h-full" />
+			</div>
 			<div className="basis-1/2 flex flex-col items-center justify-center">
 				<h1 className="text-[4rem] font-semibold mb-31">로그인</h1>
 				<a
@@ -30,14 +21,7 @@ export default function Signin() {
 						Google계정으로 로그인
 					</span>
 				</a>
-				<button
-					type="button"
-					onClick={handleAuthCheck}
-					className="mt-4 px-6 py-2 bg-blue-500 text-white rounded disabled:opacity-50"
-				>
-					인증 확인
-				</button>
 			</div>
 		</div>
-	)
+	);
 }
