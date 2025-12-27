f40318e4f7da9e67234f134e25e8fe203a1191a7
Dino0204 <s24020@gsm.hs.kr>
2025-12-28 01:12:38 +0900

    remove: mock data

--- src/entities/word/model/useSearchWord.ts
+++ src/entities/word/model/useSearchWord.ts
@@ -1,21 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
-import { MOCK_LIKES_DATA } from "@/entities/user/mock/user";
 import { getLikes } from "@/features/likes/api/getLikes";
-import { MOCK_DICTIONARY_DATA } from "@/shared/mocks/dictionary";
 import { searchWord } from "../api/searchWord";
 
 export default function useSearchWord(search: string) {
 	const { data: words } = useQuery({
 		queryKey: ["word", "search", search],
 		queryFn: async () => searchWord({ search }),
-		placeholderData: MOCK_DICTIONARY_DATA,
 		enabled: !!search,
 	});
 
 	const { data: likes } = useQuery({
 		queryKey: ["likes", "word"],
 		queryFn: async () => getLikes({}),
-		placeholderData: MOCK_LIKES_DATA,
 		enabled: !!words,
 	});
 
