6d63b5170681a0250a1df944ed679becba35da7c
Dino0204 <s24020@gsm.hs.kr>
2025-12-28 03:12:21 +0900

    fix: 이름 변경 후 UI에 반영되지 않던 문제

--- src/entities/dondon/model/useEditDonDonNickName.ts
+++ src/entities/dondon/model/useEditDonDonNickName.ts
@@ -1,8 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { editDonDonNickName } from "../api/editDonDonNickName";
 
 export const useEditDonDonNickName = () => {
+	const queryClient = useQueryClient();
+
 	return useMutation({
 		mutationFn: editDonDonNickName,
+		onSuccess: () => {
+			queryClient.invalidateQueries({
+				queryKey: ["myInfo"],
+			});
+		},
 	});
 };
